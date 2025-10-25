3f5682160c68f8a4a25fcc60885a2cc03e71e88b
silveranon323 <101815577+silveranon323@users.noreply.github.com>
2025-10-25 10:18:32 +0530

    initial commit

---
+++ restapi-tutorial/src/main/java/com/tutorial/restapi_tutorial/RestapiTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.tutorial.restapi_tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapiTutorialApplication.class, args);
+	}
+
+}
---
+++ restapi-tutorial/src/test/java/com/tutorial/restapi_tutorial/RestapiTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorial.restapi_tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
